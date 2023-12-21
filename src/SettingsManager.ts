475d5ac2c882f7d73c68f0cd2a4b12c4185ca08f
KumaV1 <KumaV1>
2023-12-21 22:30:49 +0100

    Added localization support

--- src/SettingsManager.ts
+++ src/SettingsManager.ts
@@ -3,66 +3,66 @@ import { Constants } from "./Constants";
 export class SettingsManager {
     public static init(ctx: Modding.ModContext): void {
         // Create settings
-        ctx.settings.section("Enable all").add([
+        ctx.settings.section(getLangString(`${Constants.MOD_NAMESPACE}_Settings_Section_Enable`)).add([
             {
                 type: 'label',
                 name: 'info',
-                label: "Regarding dungeons, if a monster can be found outside a dungeon, then jumping to the monster will enter the corresponding combat/slayer area. On the other hand, if the monster can only be found in a dungeon, then jumping to that monster will start the dungeon, rather than fighting the monster directly. Do note, that if a monster can be found in more than one dungeon, then the first one found will be selected (should usually be the one with lowest combat stats)."
+                label: getLangString(`${Constants.MOD_NAMESPACE}_Settings_Setting_Label_Info`),
             } as Modding.Settings.SettingConfig,
             {
                 type: 'checkbox-group',
-                name: 'force-all-non-bosses-of-area-types-valid',
-                label: "Force all monsters not categorized as boss to be viable slayer tasks",
-                hint: 'Usually already the case for slayer areas. As for dungeons, "Into the Mist", "Impending Darkeness" and "Lair of the Spider Queen" cannot be rolled for. Also, please be aware, that finishing a dungeon-monster task with auto slayer on will instantly take you out of the dungeon, without finishing the dungeon first (even if the next task is a monster only found in the same dungeon you were just fighting in).',
+                name: 'force-all-non-bosses-of-area-types-slayerable',
+                label: getLangString(`${Constants.MOD_NAMESPACE}_Settings_Setting_Label_Force_All_Non_Bosses_Of_Area_Type_Slayerable`),
+                hint: getLangString(`${Constants.MOD_NAMESPACE}_Settings_Setting_Hint_Force_All_Non_Bosses_Of_Area_Type_Slayerable`),
                 options: [
                     {
                         value: CombatAreaType.Combat,
-                        label: "Combat Areas"
+                        label: getLangString('GAME_GUIDE_COMBAT_COMBAT_AREAS')
                     },
                     {
                         value: CombatAreaType.Slayer,
-                        label: "Slayer Areas"
+                        label: getLangString('GAME_GUIDE_COMBAT_SLAYER_AREAS')
                     },
                     {
                         value: CombatAreaType.Dungeon,
-                        label: "Dungeons"
+                        label: getLangString('GAME_GUIDE_COMBAT_DUNGEONS')
                     }
                 ],
                 onChange(value: string, previousValue: string): void {
                     SettingsManager.setButtonToReload();
 
-                    const hint = document.querySelector(`label[for="${Constants.MOD_NAMESPACE}:force-all-non-bosses-of-area-types-valid"] > small`);
+                    const hint = document.querySelector(`label[for="${Constants.MOD_NAMESPACE}:force-all-non-bosses-of-area-types-slayerable"] > small`);
                     if (hint) {
-                        hint.textContent = "Reload required";
+                        hint.textContent = getLangString(`${Constants.MOD_NAMESPACE}_Settings_Hint_Save_Reload_Required`);
                         hint.classList.add("text-warning");
                     }
                 }
             } as Modding.Settings.CheckboxGroupConfig,
             {
                 type: 'checkbox-group',
-                name: 'force-all-bosses-of-area-types-valid',
-                label: "Force all monsters categorized as boss to be viable slayer tasks",
-                hint: 'Usually, only the final monster in dungeons is categorized as a boss. There may be exceptions to this rule, though, especially with modding, so you can configure this for every area type. Regarding dungeons, "Into the Mist" and "Impending Darkeness" cannot be rolled for.',
+                name: 'force-all-bosses-of-area-types-slayerable',
+                label: getLangString(`${Constants.MOD_NAMESPACE}_Settings_Setting_Label_Force_All_Bosses_Of_Area_Type_Slayerable`),
+                hint: getLangString(`${Constants.MOD_NAMESPACE}_Settings_Setting_Hint_Force_All_Bosses_Of_Area_Type_Slayerable`),
                 options: [
                     {
                         value: CombatAreaType.Combat,
-                        label: "Combat Areas"
+                        label: getLangString('GAME_GUIDE_COMBAT_COMBAT_AREAS')
                     },
                     {
                         value: CombatAreaType.Slayer,
-                        label: "Slayer Areas"
+                        label: getLangString('GAME_GUIDE_COMBAT_SLAYER_AREAS')
                     },
                     {
                         value: CombatAreaType.Dungeon,
-                        label: "Dungeons"
+                        label: getLangString('GAME_GUIDE_COMBAT_DUNGEONS')
                     }
                 ],
                 onChange(value: string, previousValue: string): void {
                     SettingsManager.setButtonToReload();
 
-                    const hint = document.querySelector(`label[for="${Constants.MOD_NAMESPACE}:force-all-bosses-of-area-types-valid"] > small`);
+                    const hint = document.querySelector(`label[for="${Constants.MOD_NAMESPACE}:force-all-bosses-of-area-types-slayerable"] > small`);
                     if (hint) {
-                        hint.textContent = "Reload required";
+                        hint.textContent = getLangString(`${Constants.MOD_NAMESPACE}_Settings_Hint_Save_Reload_Required`);
                         hint.classList.add("text-warning");
                     }
                 }
@@ -70,7 +70,7 @@ export class SettingsManager {
             {
                 type: "button",
                 name: "save-reload",
-                display: "Save & Reload",
+                display: getLangString(`${Constants.MOD_NAMESPACE}_Settings_Setting_Display_Save_Reload`),
                 color: "primary",
                 onClick: () => {
                     saveData();
@@ -210,8 +210,8 @@ export class SettingsManager {
      */
     public static getForceAllNonBossesOfAreaTypesValid(ctx: Modding.ModContext): CombatAreaType[] {
         return ctx.settings
-            .section("Enable all")
-            .get('keep-specific-monster-types-inactive') as CombatAreaType[] ?? [];
+            .section(getLangString(`${Constants.MOD_NAMESPACE}_Settings_Section_Enable`))
+            .get('force-all-non-bosses-of-area-types-slayerable') as CombatAreaType[] ?? [];
     }
 
     /**
@@ -219,8 +219,8 @@ export class SettingsManager {
      */
     public static getForceAllBossesOfAreaTypesValid(ctx: Modding.ModContext): CombatAreaType[] {
         return ctx.settings
-            .section("Enable all")
-            .get('keep-specific-monster-types-inactive') as CombatAreaType[] ?? [];
+            .section(getLangString(`${Constants.MOD_NAMESPACE}_Settings_Section_Enable`))
+            .get('force-all-bosses-of-area-types-slayerable') as CombatAreaType[] ?? [];
     }
 
     /**
---
+++ src/translation/languages/russian.ts
@@ -0,0 +1,20 @@
+export const ru = {
+    // ### Settings
+    // #### Enable All
+    Settings_Section_Enable: 'Enable',
+
+    // ##### Info
+    Settings_Setting_Label_Info: 'Regarding dungeons, if a monster can be found outside a dungeon, then jumping to the monster will enter the corresponding combat/slayer area. On the other hand, if the monster can only be found in a dungeon, then jumping to that monster will start the dungeon, rather than fighting the monster directly. Do note, that if a monster can be found in more than one dungeon, then the first one found will be selected (should usually be the one with lowest combat stats).',
+
+    // ##### Non bosses slayerable
+    Settings_Setting_Label_Force_All_Non_Bosses_Of_Area_Type_Slayerable: 'Force all monsters not categorized as boss to be viable slayer tasks',
+    Settings_Setting_Hint_Force_All_Non_Bosses_Of_Area_Type_Slayerable: 'Usually already the case for slayer areas. As for dungeons, "Into the Mist", "Impending Darkeness" and "Lair of the Spider Queen" cannot be rolled for. Also, please be aware, that finishing a dungeon-monster task with auto slayer on will instantly take you out of the dungeon, without finishing the dungeon first (even if the next task is a monster only found in the same dungeon you were just fighting in).',
+
+    // ##### Bosses slayerable
+    Settings_Setting_Label_Force_All_Bosses_Of_Area_Type_Slayerable: 'Force all monsters categorized as boss to be viable slayer tasks',
+    Settings_Setting_Hint_Force_All_Bosses_Of_Area_Type_Slayerable: 'Usually, only the final monster in dungeons is categorized as a boss. There may be exceptions to this rule, though, especially with modding, so you can configure this for every area type. Regarding dungeons, "Into the Mist" and "Impending Darkeness" cannot be rolled for.',
+
+    // ##### Save & Reload button
+    Settings_Hint_Save_Reload_Required: 'Reload required',
+    Settings_Setting_Display_Save_Reload: 'Save all & Reload'
+};
