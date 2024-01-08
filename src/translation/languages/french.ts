dc85b5b3913d843eb45d1b81397bf31c4c3f8770
KumaV1 <KumaV1>
2024-01-08 12:02:51 +0100

    Added machine translation for french

--- src/translation/languages/french.ts
+++ src/translation/languages/french.ts
@@ -1,20 +1,20 @@
-export const fr = {
+﻿export const fr = {
     // ### Settings
     // #### Enable All
-    Settings_Section_Enable: 'Enable',
+    Settings_Section_Enable: 'Activer',
 
     // ##### Info
-    Settings_Setting_Label_Info: 'Regarding dungeons, if a monster can be found outside a dungeon, then jumping to the monster will enter the corresponding combat/slayer area. On the other hand, if the monster can only be found in a dungeon, then jumping to that monster will start the dungeon, rather than fighting the monster directly. Do note, that if a monster can be found in more than one dungeon, then the first one found will be selected (should usually be the one with lowest combat stats).',
+    Settings_Setting_Label_Info: "En ce qui concerne les donjons, si un monstre peut être trouvé à l'extérieur d'un donjon, le fait de sauter sur le monstre permet d'entrer dans la zone de combat/sauvetage correspondante. En revanche, si le monstre ne peut être trouvé que dans un donjon, le fait de sauter sur ce monstre lancera le donjon, plutôt que de combattre le monstre directement. Notez que si un monstre peut être trouvé dans plus d'un donjon, c'est le premier trouvé qui sera sélectionné (il s'agit généralement de celui dont les statistiques de combat sont les plus faibles).",
 
     // ##### Non bosses slayerable
-    Settings_Setting_Label_Force_All_Non_Bosses_Of_Area_Type_Slayerable: 'Force all monsters not categorized as boss to be viable slayer tasks',
-    Settings_Setting_Hint_Force_All_Non_Bosses_Of_Area_Type_Slayerable: 'Usually already the case for slayer areas. As for dungeons, "Into the Mist", "Impending Darkeness", "Lair of the Spider Queen" and "Throne of the Herald" cannot be rolled for. Also, please be aware, that finishing a dungeon-monster task with auto slayer on will instantly take you out of the dungeon, without finishing the dungeon first (even if the next task is a monster only found in the same dungeon you were just fighting in).',
+    Settings_Setting_Label_Force_All_Non_Bosses_Of_Area_Type_Slayerable: 'Obliger tous les monstres qui ne sont pas classés comme boss à être des tâches de tueur viables',
+    Settings_Setting_Hint_Force_All_Non_Bosses_Of_Area_Type_Slayerable: "C'est généralement déjà le cas pour les zones de tueurs. En ce qui concerne les donjons, \"Dans la brume\", \"L'obscurité imminente\", \"Le repaire de la reine des araignées\" et \"Trône du Héraut\" ne peuvent pas faire l'objet d'un jet de dé. De plus, sachez que terminer une tâche de donjon-monstre avec l'auto slayer activé vous fera instantanément sortir du donjon, sans avoir fini le donjon en premier (même si la prochaine tâche est un monstre que l'on ne trouve que dans le même donjon que celui dans lequel vous venez de vous battre).",
 
     // ##### Bosses slayerable
-    Settings_Setting_Label_Force_All_Bosses_Of_Area_Type_Slayerable: 'Force all monsters categorized as boss to be viable slayer tasks',
-    Settings_Setting_Hint_Force_All_Bosses_Of_Area_Type_Slayerable: 'Usually, only the final monster in dungeons is categorized as a boss. There may be exceptions to this rule, though, especially with modding, so you can configure this for every area type. Regarding dungeons, "Into the Mist", "Impending Darkeness" and "Throne of the Herald" cannot be rolled for.',
+    Settings_Setting_Label_Force_All_Bosses_Of_Area_Type_Slayerable: 'Obliger tous les monstres catégorisés comme boss à être des tâches de tueur viables',
+    Settings_Setting_Hint_Force_All_Bosses_Of_Area_Type_Slayerable: "En général, seul le dernier monstre des donjons est considéré comme un boss. Il peut cependant y avoir des exceptions à cette règle, en particulier dans le cadre de la modélisation, et vous pouvez donc configurer cela pour chaque type de zone. En ce qui concerne les donjons, \"Dans la brume\", \"L'obscurité imminente\" et \"Trône du Héraut\" ne peuvent pas faire l'objet d'un jet de dé.",
 
     // ##### Save & Reload button
-    Settings_Hint_Save_Reload_Required: 'Reload required',
-    Settings_Setting_Display_Save_Reload: 'Save all & Reload'
+    Settings_Hint_Save_Reload_Required: 'Recharge nécessaire',
+    Settings_Setting_Display_Save_Reload: 'Enregistrer tout et recharger'
 };
