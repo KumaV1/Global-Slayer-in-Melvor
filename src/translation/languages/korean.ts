f68e2d0d664590e7d4c22e7f744e4c7536e8cce7
KumaV1 <KumaV1>
2024-01-08 12:14:14 +0100

    Added machine translation for korean

--- src/translation/languages/korean.ts
+++ src/translation/languages/korean.ts
@@ -1,20 +1,20 @@
-export const ko = {
+﻿export const ko = {
     // ### Settings
     // #### Enable All
-    Settings_Section_Enable: 'Enable',
+    Settings_Section_Enable: '사용',
 
     // ##### Info
-    Settings_Setting_Label_Info: 'Regarding dungeons, if a monster can be found outside a dungeon, then jumping to the monster will enter the corresponding combat/slayer area. On the other hand, if the monster can only be found in a dungeon, then jumping to that monster will start the dungeon, rather than fighting the monster directly. Do note, that if a monster can be found in more than one dungeon, then the first one found will be selected (should usually be the one with lowest combat stats).',
+    Settings_Setting_Label_Info: '던전의 경우, 던전 밖에서 몬스터를 발견할 수 있는 경우 해당 몬스터에게 점프하면 해당 전투/플레이어 영역으로 들어갑니다. 반면, 던전 내에서만 몬스터를 찾을 수 있는 경우, 몬스터와 직접 싸우지 않고 해당 몬스터에게 점프하면 던전이 시작됩니다. 한 몬스터가 두 개 이상의 던전에서 발견될 수 있는 경우, 가장 먼저 발견되는 몬스터가 선택됩니다(일반적으로 전투 능력치가 가장 낮은 몬스터가 선택됩니다).',
 
     // ##### Non bosses slayerable
-    Settings_Setting_Label_Force_All_Non_Bosses_Of_Area_Type_Slayerable: 'Force all monsters not categorized as boss to be viable slayer tasks',
-    Settings_Setting_Hint_Force_All_Non_Bosses_Of_Area_Type_Slayerable: 'Usually already the case for slayer areas. As for dungeons, "Into the Mist", "Impending Darkeness", "Lair of the Spider Queen" and "Throne of the Herald" cannot be rolled for. Also, please be aware, that finishing a dungeon-monster task with auto slayer on will instantly take you out of the dungeon, without finishing the dungeon first (even if the next task is a monster only found in the same dungeon you were just fighting in).',
+    Settings_Setting_Label_Force_All_Non_Bosses_Of_Area_Type_Slayerable: '보스로 분류되지 않은 모든 몬스터가 생존 가능한 처치 과제가 되도록 강제 설정',
+    Settings_Setting_Hint_Force_All_Non_Bosses_Of_Area_Type_Slayerable: '일반적으로 슬레이어 지역에서는 이미 적용됩니다. 던전의 경우, "안개 속으로", "임박한 어둠", "거미 여왕의 은신처", "전령의 왕좌"는 롤링할 수 없습니다. 또한, 자동 처치 기능을 켠 상태에서 던전 몬스터 과제를 완료하면 해당 던전을 먼저 완료하지 않아도 즉시 던전에서 빠져나갈 수 있습니다(다음 과제가 방금 싸운 던전에서만 볼 수 있는 몬스터인 경우에도 마찬가지입니다).',
 
     // ##### Bosses slayerable
-    Settings_Setting_Label_Force_All_Bosses_Of_Area_Type_Slayerable: 'Force all monsters categorized as boss to be viable slayer tasks',
-    Settings_Setting_Hint_Force_All_Bosses_Of_Area_Type_Slayerable: 'Usually, only the final monster in dungeons is categorized as a boss. There may be exceptions to this rule, though, especially with modding, so you can configure this for every area type. Regarding dungeons, "Into the Mist", "Impending Darkeness" and "Throne of the Herald" cannot be rolled for.',
+    Settings_Setting_Label_Force_All_Bosses_Of_Area_Type_Slayerable: '보스로 분류된 모든 몬스터가 생존 가능한 처치 과제를 수행하도록 합니다.',
+    Settings_Setting_Hint_Force_All_Bosses_Of_Area_Type_Slayerable: '일반적으로 던전의 최종 몬스터만 보스로 분류됩니다. 하지만 이 규칙에는 예외가 있을 수 있으며, 특히 모딩의 경우 모든 지역 유형에 대해 이를 설정할 수 있습니다. 던전의 경우, "안개 속으로", "임박한 어둠", "전령의 왕좌"는 롤링할 수 없습니다.',
 
     // ##### Save & Reload button
-    Settings_Hint_Save_Reload_Required: 'Reload required',
-    Settings_Setting_Display_Save_Reload: 'Save all & Reload'
+    Settings_Hint_Save_Reload_Required: '재장전 필요',
+    Settings_Setting_Display_Save_Reload: '모두 저장 및 다시 로드'
 };
