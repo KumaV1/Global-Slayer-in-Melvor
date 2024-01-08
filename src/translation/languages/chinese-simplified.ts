72ed63a8ab87d884810ea8e0a60f80bca495414d
KumaV1 <KumaV1>
2024-01-08 11:53:30 +0100

    Added machine translation for simplified chinese

--- src/translation/languages/chinese-simplified.ts
+++ src/translation/languages/chinese-simplified.ts
@@ -1,20 +1,20 @@
-export const zhCN = {
+﻿export const zhCN = {
     // ### Settings
     // #### Enable All
-    Settings_Section_Enable: 'Enable',
+    Settings_Section_Enable: '启用',
 
     // ##### Info
-    Settings_Setting_Label_Info: 'Regarding dungeons, if a monster can be found outside a dungeon, then jumping to the monster will enter the corresponding combat/slayer area. On the other hand, if the monster can only be found in a dungeon, then jumping to that monster will start the dungeon, rather than fighting the monster directly. Do note, that if a monster can be found in more than one dungeon, then the first one found will be selected (should usually be the one with lowest combat stats).',
+    Settings_Setting_Label_Info: '关于地牢，如果可以在地牢外找到怪物，那么跳到该怪物处将进入相应的战斗/屠宰区域。另一方面，如果怪物只能在地牢中找到，那么跳到该怪物处就会开始进入地牢，而不是直接与怪物战斗。请注意，如果一个怪物可以在多个地牢中找到，那么将选择第一个找到的怪物（通常应该是战斗属性最低的那个）。',
 
     // ##### Non bosses slayerable
-    Settings_Setting_Label_Force_All_Non_Bosses_Of_Area_Type_Slayerable: 'Force all monsters not categorized as boss to be viable slayer tasks',
-    Settings_Setting_Hint_Force_All_Non_Bosses_Of_Area_Type_Slayerable: 'Usually already the case for slayer areas. As for dungeons, "Into the Mist", "Impending Darkeness", "Lair of the Spider Queen" and "Throne of the Herald" cannot be rolled for. Also, please be aware, that finishing a dungeon-monster task with auto slayer on will instantly take you out of the dungeon, without finishing the dungeon first (even if the next task is a monster only found in the same dungeon you were just fighting in).',
+    Settings_Setting_Label_Force_All_Non_Bosses_Of_Area_Type_Slayerable: '强制所有未被归类为 BOSS 的怪物执行可行的屠夫任务',
+    Settings_Setting_Hint_Force_All_Non_Bosses_Of_Area_Type_Slayerable: '通常情况下，杀手区已经是这样了。至于地下城，"进入迷雾"、"即将到来的黑暗"、"蜘蛛女王的巢穴 "和 "先驱者的王座 "是无法滚动的。此外，请注意，在开启自动杀怪功能的情况下完成地牢怪物任务，会立即带您离开地牢，而无需先完成地牢（即使下一个任务是您刚刚在同一个地牢中战斗过的怪物）。',
 
     // ##### Bosses slayerable
-    Settings_Setting_Label_Force_All_Bosses_Of_Area_Type_Slayerable: 'Force all monsters categorized as boss to be viable slayer tasks',
-    Settings_Setting_Hint_Force_All_Bosses_Of_Area_Type_Slayerable: 'Usually, only the final monster in dungeons is categorized as a boss. There may be exceptions to this rule, though, especially with modding, so you can configure this for every area type. Regarding dungeons, "Into the Mist", "Impending Darkeness" and "Throne of the Herald" cannot be rolled for.',
+    Settings_Setting_Label_Force_All_Bosses_Of_Area_Type_Slayerable: '强制所有归类为 BOSS 的怪物执行可行的杀戮任务',
+    Settings_Setting_Hint_Force_All_Bosses_Of_Area_Type_Slayerable: '通常，只有地牢中的最终怪物才会被归类为 BOSS。不过这条规则也可能有例外，尤其是在进行修改的情况下，因此您可以为每种区域类型进行配置。关于地下城，"进入迷雾"、"即将到来的黑暗 "和 "先驱者的王座 "无法滚动。',
 
     // ##### Save & Reload button
-    Settings_Hint_Save_Reload_Required: 'Reload required',
-    Settings_Setting_Display_Save_Reload: 'Save all & Reload'
+    Settings_Hint_Save_Reload_Required: '需要重新加载',
+    Settings_Setting_Display_Save_Reload: '全部保存并重新加载'
 };
